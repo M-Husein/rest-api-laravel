35d9afcdab1ea0b3f2a14241e4c3f421a6d0081f
M-Husein <m.husein27@gmail.com>
2025-05-28 21:26:35 +0700

    feat: tidy up

--- README.md
+++ README.md
@@ -67,11 +67,11 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
 
-# CUSTOM
+# 
 
 # Custom CLI
 
-For generate resouce, 
+To generate resource.
 
 Example usage:
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,33 +2,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Auth\AuthenticationException;
+// use Illuminate\Auth\AuthenticationException;
 use Illuminate\Routing\Router;
 
 class AppServiceProvider extends ServiceProvider{
     /**
      * Register any application services.
      */
-    public function register(): void{
-        
-    }
+    public function register(): void{}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void{
-        Router::macro('customUnauthorizedResponse', function(AuthenticationException $exception){
-            return response()->json([
+        Router::macro('customUnauthorizedResponse', fn() => // AuthenticationException $exception
+            response()->json([
                 'code' => 401,
-                'errors' => 'unauthorized',
-                // 'timestamp' => now()->toISOString()
-            ], 401);
-        });
-
-        // Apply to all routes
-        $this->app->bind(
-            \Illuminate\Contracts\Debug\ExceptionHandler::class,
-            \App\Exceptions\Handler::class
+                'errors' => 'unauthorized'
+            ], 401)
         );
+        // Apply to all routes
+        $this->app->bind(\Illuminate\Contracts\Debug\ExceptionHandler::class, \App\Exceptions\Handler::class);
     }
 }
